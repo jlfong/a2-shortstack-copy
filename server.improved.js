e95dc03a6d7a9699fdeb34079a0f2814a5d6fe85
Glitch (jlfong-a2-shortstack-copy) <none>
2019-09-15 01:11:03 +0000

    👩🚘 Checkpoint ./server.improved.js:6342919/3158

--- server.improved.js
+++ server.improved.js
@@ -101,7 +101,7 @@ app.post('/test', function( req, res ) {
 
 app.get('/studentData', (req, res) => {
   let data = db.get('appdata').value()
-  console.log(data)
+  //console.log(data)
   res.send(data);
   //res.send(appdata);
 });
@@ -132,14 +132,15 @@ app.post('/submit', function(req, res) {
 
 app.post('/update', function(req, res) {
   const index = req.body.index,
-        indexVal = db.get('appData['+index+']').value()
-  console.log(indexVal)
-  db.get('appdata['+index+']').assign({
-    firstName: indexVal.firstName,
-    lastName: indexVal.lastName,
-    pronouns: indexVal.pronouns,
-    house: indexVal.house
+        indexVal = db.get('appdata['+index+']').value()
+  console.log(indexVal.house)
+  db.get('appdata').find(indexVal).assign({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    pronouns: req.body.pronouns,
+    house: req.body.house
   }).write()
+  console.log(db.get('appdata').find(indexVal).house)
   /*var data = req.body
   let index = data.index
   appdata[index].firstName = data.firstName
@@ -172,103 +173,6 @@ const appdata = [
     {'firstName': 'Jose', 'lastName': 'Li Quiel', 'pronouns': 'He/Him/His', 'values': 'loyalty', 'house': 'Hufflepuff'},
     {'firstName': 'Harry', 'lastName': 'Potter', 'pronouns': 'He/Him/His', 'values': 'bravery', 'house': 'Gryffindor'},
 ]
-
-
-const server = http.createServer(function (request, response) {
-    if (request.method === 'GET') {
-        handleGet(request, response)
-    } else if (request.method === 'POST') {
-        handlePost(request, response)
-    }
-})
-
-
-const handleGet = function (request, response) {
-    const filename = dir + request.url.slice(1)
-
-    if (request.url === '/') {
-        sendFile(response, 'public/index.html')
-    } else if (request.url === '/studentData') {
-        sendData(response, appdata)
-    } else {
-        sendFile(response, filename)
-    }
-}
-
-const handlePost = function (request, response) {
-    let dataString = ''
-
-    request.on('data', function (data) {
-        dataString += data
-    })
-
-    request.on('end', function () {
-
-            const data = JSON.parse(dataString)
-
-
-            switch (request.url) {
-
-                case '/submit':
-                    //server logic
-                    let sortedHouse;
-                    switch (data.values) {
-                        case 'bravery':
-                            sortedHouse = 'Gryffindor'
-                            break
-                        case 'loyalty':
-                            sortedHouse = 'Hufflepuff'
-                            break
-                        case 'wisdom':
-                            sortedHouse = 'Ravenclaw'
-                            break
-                        case 'ambition':
-                            sortedHouse = 'Slytherin'
-                            break
-                        default:
-                            sortedHouse = 'Muggle'
-                    }
-
-                    const newStudent = {
-                        'firstName': data.firstName,
-                        'lastName': data.lastName,
-                        'pronouns': data.pronouns,
-                        'values': data.values,
-                        'house': sortedHouse
-                    }
-
-                    appdata.push(newStudent)
-
-                    console.log(appdata)
-
-                    response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
-                    response.end()
-                    break
-
-                case '/delete':
-                    appdata.splice(data.rowData, 1)
-                    response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
-                    response.end()
-                    break
-
-                case '/update':
-                    let index = data.index
-                    appdata[index].firstName = data.firstName
-                    appdata[index].lastName = data.lastName
-                    appdata[index].pronouns = data.pronouns
-                    appdata[index].house = data.house
-                    response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
-                    response.end()
-                    break
-
-                default:
-                    response.end('404 Error: File Not Found')
-            }
-
-
-        }
-    )
-}
 
 const sendData = function (response, studentData) {
     response.end(JSON.stringify(studentData));
